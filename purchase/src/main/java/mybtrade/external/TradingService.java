224400acc78b5e72eac49fc86ed0b55a5e252f85
smilekan <tmakdlf74!)>
2020-07-15 18:46:15 +0900

    Feign External Ip 설정수정

--- purchase/src/main/java/mybtrade/external/TradingService.java
+++ purchase/src/main/java/mybtrade/external/TradingService.java
@@ -2,16 +2,17 @@
 package mybtrade.external;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
-@FeignClient(name="trading", url="${api.url.mybtrade}")
+@FeignClient(name="trading", url="http://trade:8080")
 public interface TradingService {
 
     @RequestMapping(method= RequestMethod.PATCH, path="/tradings")
-    public void purchasecancel(@RequestBody Trading trading);
+    @ResponseBody
+    Trading tradecancel(@RequestParam Long salesNum);
+
+//    public void purchasecancel(@RequestBody Trading trading);
 
 }
